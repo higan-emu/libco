9a21f2854517ed4e7bf41a86b2d371d23ce174ba
Björn Svensson <bjorn.a.svensson@est.tech>
2023-04-18 17:05:04 +0200

    Build test_serialization with c++11
    test_serialization.cpp uses `auto` which requires c++11.
    Needed on macOS that uses its Apple Clang (default c++98).

--- doc/examples/build.sh
+++ doc/examples/build.sh
@@ -3,6 +3,6 @@ c++ -O3 -fomit-frame-pointer -I../.. -c test_timing.cpp
 c++ -O3 -fomit-frame-pointer -o test_timing libco.o test_timing.o
 c++ -O3 -fomit-frame-pointer -I../.. -c test_args.cpp
 c++ -O3 -fomit-frame-pointer -o test_args libco.o test_args.o
-c++ -O3 -fomit-frame-pointer -I../.. -c test_serialization.cpp
+c++ -O3 -fomit-frame-pointer -std=c++11 -I../.. -c test_serialization.cpp
 c++ -O3 -fomit-frame-pointer -o test_serialization libco.o test_serialization.o
 rm -f *.o
