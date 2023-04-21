7191c5a6be938ea0e3773d199496b6d038b80abd
Björn Svensson <bjorn.a.svensson@est.tech>
2023-04-21 08:58:08 +0200

    Use malloc in test_serialization
    This enables the test to be compiled on Windows and run on
    arm, x86 and Windows, including with ASan.

--- doc/examples/test_serialization.cpp
+++ doc/examples/test_serialization.cpp
@@ -1,6 +1,5 @@
 #include "test.h"
 #include <stdint.h>
-#include <sys/mman.h>
 
 namespace Thread {
   cothread_t host;
@@ -80,11 +79,7 @@ auto main() -> int {
     return 1;
   }
 
-  Memory::buffer = (uint8_t*)mmap(
-    (void*)0x10'0000'0000, 2 * 65536,
-    PROT_READ | PROT_WRITE | PROT_EXEC,
-    MAP_FIXED | MAP_PRIVATE | MAP_ANONYMOUS, -1, 0
-  );
+  Memory::buffer = (uint8_t*)malloc(2 * 65536);
   Memory::buffer[0] = 42;
   printf("%p (%u)\n", Memory::buffer, Memory::buffer[0]);
 
@@ -112,6 +107,6 @@ auto main() -> int {
 
   Thread::cpu = nullptr;
   Thread::apu = nullptr;
-  munmap((void*)0x900000000, 2 * 65536);
+  free(Memory::buffer);
   return 0;
 }
